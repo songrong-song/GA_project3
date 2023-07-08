f79404a6dd4a857f64e9a90bf869a72fd2379135
Songrong <wusongrong.song@gmail.com>
2023-07-09 07:33:38 +0800

    add BE-Intenary controller

--- BE/server.js
+++ BE/server.js
@@ -30,6 +30,7 @@ app.options('*', cors())
 
 app.use('/api/users', userRouter)
 app.use('/api/gpt', GPTRouter)
+app.use('/api/intenary', intenaryRouter)
 
 // LISTENER
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
