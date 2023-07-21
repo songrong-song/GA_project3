6a9655f1d70d5f441a4acd85541954e25ddf3b01
Songrong <wusongrong.song@gmail.com>
2023-07-22 03:02:22 +0800

    be

--- BE/controllers/userItinerary_controller.js
+++ BE/controllers/userItinerary_controller.js
@@ -31,18 +31,19 @@ const userControllers = {
     saveResult: async (req, res) => {
       
       try {
-        console.log("here")
-        console.log(req.body)
+        
+    
         // Extract the necessary data from the request
         const { userID, destinationValue, dayValue, itinerary } = req.body;
-    
+
+
         // Create a new UserItinerary instance
         const userItinerary = new userItineraryModel({
 
           userId: userID,
           dayValue: dayValue,
           destination:  destinationValue,
-          itineraries:  itinerary,
+          itineraries:  JSON.parse(itinerary),
         
         });
     
@@ -56,11 +57,25 @@ const userControllers = {
         console.error('Error saving user itinerary:', error);
         res.status(500).json({ message: 'An error occurred while saving user itinerary' });
       }
-    
-    } 
-        
+    },
 
-
+    deleteAllItinerariesByUserId: async (req, res) => {
+      // Get the userID from the request parameters
+      const { userId } = req.body;
+      console.log(userId)
+  
+      try {
+        // Delete all records with the given userId from the user itinerary collection
+        await userItineraryModel.deleteMany({ userId: userId });
+  
+        // Send a success response
+        res.status(200).json({ message: 'All itineraries for the specific user deleted successfully' });
+      } catch (error) {
+        // Handle any errors and send an error response
+        console.error('Error deleting itineraries for the specific user:', error);
+        res.status(500).json({ message: 'An error occurred while deleting itineraries for the specific user' });
+      }
+    },
    
 }
 
--- BE/routers/userItinerary_router.js
+++ BE/routers/userItinerary_router.js
@@ -4,5 +4,6 @@ const userItineraryController = require('../controllers/userItinerary_controller
 
 router.post('/', userItineraryController.fetchHistoricalResult)
 router.post('/saveItinerary', userItineraryController.saveResult)
+router.post('/deleteAllItinerary', userItineraryController.deleteAllItinerariesByUserId)
 
 module.exports = router
