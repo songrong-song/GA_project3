130c9a009ae59df3b1e51c570c91a7efd67ffa0f
Songrong <wusongrong.song@gmail.com>
2023-07-21 15:51:06 +0800

    worked on saved profile, to solve the conflict

--- BE/controllers/userItinerary_controller.js
+++ BE/controllers/userItinerary_controller.js
@@ -1,7 +1,7 @@
 const Joi = require("joi")
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
-const userItineraryModel = require("../models/UserItineraryModel")
+const UserItinerary = require("../models/UserItineraryModel")
 const userValidators = require("./validators/userValidator")
 
 
@@ -28,24 +28,29 @@ const userControllers = {
     },
 
     saveResult: async (req, res) => {
-        // get the registration data in the req
-        const data = req.body;
-        console.log(data);
-        var userEmail = data.userEmail;
-
-        try {
-            const result = await userItineraryModel.find({ userEmail: userEmail })
-            if (result) {
-              // User found
-              res.json(result);
-            } else {
-              // User not found
-              res.status(404).json({ error: "Itinerary not found" });
-            }
-          } catch (error) {
-            console.error('Error finding user:', error);
-            res.status(500).json({ error: 'Fail to return the Itinerary' });
-          }
+      
+      try {
+        // Extract the necessary data from the request
+        const { userId, destination, itineraries } = req.body;
+    
+        // Create a new UserItinerary instance
+        const userItinerary = new UserItinerary({
+          userId,
+          destination,
+          itineraries,
+        });
+    
+        // Save the UserItinerary to the database
+        await userItinerary.save();
+    
+        // Send a success response
+        res.status(200).json({ message: 'User itinerary saved successfully' });
+      } catch (error) {
+        // Handle any errors and send an error response
+        console.error('Error saving user itinerary:', error);
+        res.status(500).json({ message: 'An error occurred while saving user itinerary' });
+      }
+    
     } 
         
 
