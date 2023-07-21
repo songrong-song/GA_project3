5d751d7c392d6d9933cdef39b06fc318f5ceba9e
sandyyeo93 <126796964+sandyyeo93@users.noreply.github.com>
2023-07-21 12:20:47 +0800

    css of steps, validation of destination input

--- src/Components/DestinationInput.js
+++ src/Components/DestinationInput.js
@@ -67,7 +67,7 @@ const DestinationInput = () => {
         <Col xs={24} sm={12}>
 
           <div className="input-group">
-            <label htmlFor="destination">Destination (required):</label>
+            <label htmlFor="destination">Destination:</label>
             <Form.Item
               name="destination"
               rules={[
@@ -89,10 +89,8 @@ const DestinationInput = () => {
 
         </Col>
         <Col xs={24} sm={12}>
-
           <div className="input-group">
             <label htmlFor="numDays">Number of Days (required):</label>
-
             <Form.Item
               name="numDays"
               rules={[
@@ -104,33 +102,7 @@ const DestinationInput = () => {
                 {dayOptions}
               </Select>
             </Form.Item>
-          </div> */}
-
-   
-          <div className="input-group">
-            <label htmlFor="numDays">NumberOfDays:</label>
-            <Form.Item
-              name="dayValue"
-              rules={[
-                { required: false, message: 'Please select the number of days' }
-              ]}
-            >
-              <Select
-                id="durationValue"
-                className="destination-input-field"
-                placeholder="Select number of days"
-                onChange={handleNumDaysChange}
-                value={durationValue}
-              >
-                {numberOptions.map(option => (
-                  <Option key={option} value={option}>{option}</Option>
-                ))}
-              </Select>
-            </Form.Item>
           </div>
-
-
-
         </Col>
       </Row>
     </div>
